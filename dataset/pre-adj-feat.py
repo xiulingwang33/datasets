5946825974565b262e4f4aafe393015de0ec4aa7
xiulingwang33 <84334716+xiulingwang33@users.noreply.github.com>
2021-05-19 18:58:46 -0400

    Add files via upload

--- dataset/pre-adj-feat.py
+++ dataset/pre-adj-feat.py
@@ -1,6 +1,10 @@
+import pickle as pk
+import networkx as nx
 DATASET = 'facebook'
 # DATASET='g+'
 # DATASET='dblp'
+
+###### "ginfo" in the following are to extract the gender information for each node in graph
 
 
 G_EGO_USERS=['3980']
@@ -38,26 +42,26 @@ for ego_user in G_EGO_USERS:
         f.close()
         #g1_index=invert_index[('gender',77)]
         #g2_index = invert_index[('gender', 78)]
-        g_index = invert_index[('gender', 77)]
+        gindex = invert_index[('gender', 77)]
 
         for i, n in enumerate(g.nodes()):
             info = ft[n][gindex:gindex + 2].tolist()
 
-    if sum(info) == 0:
-        ginfo = 0
-        # rm.append(n)
-    elif sum(info) == 1:
-        ginfo = info.index(1) + 1
-        if ginfo == 1:
-            count_nodes[0] += 1
-        elif ginfo == 2:
-            count_nodes[1] += 1
-    else:
-        # print(n)
-        ginfo = -1
-        # rm.append(n)
-    g.nodes[n]['gender'] = ginfo
-    print(g.nodes[1])
+            if sum(info) == 0:
+                ginfo = 0
+                # rm.append(n)
+            elif sum(info) == 1:
+                ginfo = info.index(1) + 1
+                # if ginfo == 1:
+                #     count_nodes[0] += 1
+                # elif ginfo == 2:
+                #     count_nodes[1] += 1
+            else:
+                # print(n)
+                ginfo = -1
+                # rm.append(n)
+            g.nodes[n]['gender'] = ginfo
+            print(g.nodes[1])
 
 
 
