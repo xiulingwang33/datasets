157583f92067d2d3dd6ffe3debc6335f4591b020
xiulingwang33 <84334716+xiulingwang33@users.noreply.github.com>
2021-05-20 00:59:20 -0400

    Add files via upload

--- dataset/pre-adj-feat.py
+++ dataset/pre-adj-feat.py
@@ -1,18 +1,18 @@
 import pickle as pk
 import networkx as nx
-DATASET = 'facebook'
+DATASET = 'fb'
 # DATASET='g+'
 # DATASET='dblp'
 
 ###### "ginfo" in the following are to extract the gender information for each node in graph
 
 
-G_EGO_USERS=['3980']
+G_EGO_USERS=['combined','0','106']
 
 
 for ego_user in G_EGO_USERS:
 
-    feat_dir = './' +DATASET +'/'+ str(ego_user) + '-adj-feat.pkl'
+    feat_dir = './' + str(ego_user) + '-adj-feat.pkl'
     f2 = open(feat_dir, 'rb')
 
     adj, ft = pk.load(f2, encoding='latin1')
@@ -28,40 +28,55 @@ for ego_user in G_EGO_USERS:
 
     if (DATASET == 'g+'):# gplus
 
-        for i, n in enumerate(g.nodes()):
-            info = ft[n][:3].tolist()
+        gindex=0
 
     elif(DATASET == 'dblp'):# dblp
 
-        for i, n in enumerate(g.nodes()):
-            info = ft[n].tolist()
+        gindex=0
 
     elif (DATASET == 'fb'): # facebook
-        f=open('./' + DATASET +'/' +'feature_map.txt','r')
-        invert_index=eval(f.readline())
-        f.close()
-        #g1_index=invert_index[('gender',77)]
-        #g2_index = invert_index[('gender', 78)]
-        gindex = invert_index[('gender', 77)]
+        if ego_user !='combined':
+            # featurename
+            featname_dir = './' + str(ego_user) + '.featnames'
+            # facebook feature map
+            f = open(featname_dir)
+            featnames = []
+            for line in f:
+                line = line.strip().split(' ')
+                feats = line[1]
+                feats = feats.split(';')
+                feat = feats[0]
+                featnames.append(feat)
+            # print(featnames)
+            # exit()
+            f.close()
+
+            # gender 77, gender 78
+            gindex = featnames.index('gender')
+            print(gindex)
+            # rm = []
+            #exit()
+        else:
+            gindex=77
+
+
 
         for i, n in enumerate(g.nodes()):
-            info = ft[n][gindex:gindex + 2].tolist()
+            if (ft[n][gindex]==1 and ft[n][gindex+1]!=1):
+                ginfo = 1 #male
+            elif (ft[n][gindex+1]==1and ft[n][gindex]!=1):
+                ginfo = 2 #female
 
-            if sum(info) == 0:
-                ginfo = 0
-                # rm.append(n)
-            elif sum(info) == 1:
-                ginfo = info.index(1) + 1
-                # if ginfo == 1:
-                #     count_nodes[0] += 1
-                # elif ginfo == 2:
-                #     count_nodes[1] += 1
             else:
-                # print(n)
-                ginfo = -1
-                # rm.append(n)
+                print('***')
+                ginfo = 0 #unknow gender
+
+            print(ginfo)
+
             g.nodes[n]['gender'] = ginfo
-            print(g.nodes[1])
+        #print(g.nodes[1])
+
+
 
 
 
